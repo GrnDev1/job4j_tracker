821a43b4a5b2f0ecc79d20bdef279d1e3e40b711
GrnDev1 <grndev@mail.ru>
2023-01-09 12:21:19 +0300

    2. Коллекции, ArrayList, List, Обобщения [#10094 #378958]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -8,7 +8,7 @@ public class UsageArrayList {
         names.add("Petr");
         names.add("Ivan");
         names.add("Stepan");
-        for (Object temp : names) {
+        for (String temp : names) {
             System.out.println(temp);
         }
     }
