fbbfa4048eafdf5cbe030449ce45d228ddddb475
Roman <grndev@mail.ru>
2023-12-17 00:46:37 +0300

    Added ManyToOne

--- src/main/java/ru/job4j/toone/User.java
+++ src/main/java/ru/job4j/toone/User.java
@@ -5,6 +5,8 @@ import lombok.EqualsAndHashCode;
 import lombok.EqualsAndHashCode.Include;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -21,4 +23,8 @@ public class User {
     @ManyToOne
     @JoinColumn(name = "role_id")
     private Role role;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "j_user_id")
+    private List<UserMessenger> messengers = new ArrayList<>();
 }
